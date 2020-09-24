43c2878a4f380c24fea7271bb387395e5e4030df
bdshakhawat <mdshakwat.hossain@gmail.com>
2020-09-23 22:28:48 -0700

    my comitt

---
+++ if.js
@@ -0,0 +1,9 @@
+var num = 32;
+// if (num % 2 == 0) {
+//     console.log("Even");
+// }
+// else {
+//     console.log("Odd");
+// }
+var result = num % 2 == 0 ? "Even" : "Odd";
+console.log(result);
--- vari.js
+++ vari.js
@@ -1,9 +1,13 @@
-// var x= 100;
-// x+200;
-// console.log(x+200);
+var num = 100;
+x+200;
+console.log(x+200);
 // var weather = "rainy";
 //   weather = "Sunny";
 //   weather;
 // console.log(weather);
-var x = 10+5+3;
-console.log(x);
+// var x = 10+5+3;
+// console.log(x);
+// if (num%2 == 0) 
+//     console.log("Even");
+// if (num%2 !== 0 )
+//         console.log("Odd");
