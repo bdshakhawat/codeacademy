5af2c1b982ff876a3aa537044979e4d6bc4a4392
bdshakhawat <mdshakwat.hossain@gmail.com>
2020-10-18 03:15:53 +0600

    svn commit

---
+++ function.js
@@ -0,0 +1,57 @@
+//  declaration of function
+//  function name(parameter){
+//       code Block
+//  }
+// calling and setting argument
+// function name(argument)
+
+// function adder(a,b){
+//     console.log(a+b);
+// }
+// adder(10,15);
+// function greet(a,b){
+//     console.log("Hello "  +  a   +   b);
+// }
+// greet( "Sumon" , "Khan");
+// function gre(x="John",y=" Keats"){
+//     // if(typeof x === "undefined"){
+//     //     x = "Jhon";
+//     // } 
+//     //  if (typeof y === "undefined"){
+//     //     y = " Keats";
+//     // }
+//         console.log("Hello " +  x +  y);
+// }
+// gre();
+// IIEFs function
+// (function(name){
+//     console.log(name);
+// })("sumon");
+// function calcSquare(){
+//     var num = 5;
+//     result = num * num;
+//     console.log("Result = " + result );
+// }
+// calcSquare();
+// function calcSquare(num){
+    
+//     result = num * num;
+//     console.log("Result = " + result );
+// }
+// calcSquare(3);
+// calcSquare(4);
+// calcSquare(5);
+// calcSquare(6);
+// function calcMultiple(num1, num2){
+//     result = num1 * num2;
+//     console.log("Result =  " + result);
+// }
+// calcMultiple(2,3);
+// calcMultiple(3,4);
+// calcMultiple(4,5);
+// calcMultiple(5,6);
+function multiplication(num1, num2){
+    return num1 * num2; 
+}
+// var result = multiplication(5,6);
+console.log(multiplication(5,10));
